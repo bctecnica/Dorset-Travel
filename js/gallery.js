88adb6f928c7069751164893d0c41ec59fb62364
ben <jammydude7@googlemail.com>
2022-03-11 17:47:03 +0000

    added changing text to snapshots

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,7 @@
+// cycles through each picture and corresponding location text and fades them both one after the other on the polaroid.
+
 $("#slideshow > div:gt(0)").hide();
+$('#snapshot-text p:gt(0)').hide();
 
 setInterval(function() { 
   $('#slideshow > div:first')
@@ -7,4 +10,8 @@ setInterval(function() {
   .fadeIn(1000)
   .end()
   .appendTo('#slideshow');
+  $('#snapshot-text p:first-child')
+  .fadeOut(1000)
+  .next('p').fadeIn(1000)
+  .end().appendTo('#snapshot-text');
 }, 4000);
