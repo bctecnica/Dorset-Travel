075466b58aa78dbbf7bd74149a36a8efac216010
ben <jammydude7@googlemail.com>
2022-04-11 11:45:48 +0100

    form now resets after submission

--- js/form.js
+++ js/form.js
@@ -1,35 +1,13 @@
 // function ValidateEmail(inputText)
 // {
-//     console.log(inputText)
-// var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-// if(inputText.value.match(mailformat))
-// {
-// alert("Valid email address!");
-// document.form1.text1.focus();
-// return true;
-// }
-// else
-// {
-// alert("You have entered an invalid email address!");
-// document.form1.text1.focus();
-// return false;
-// }
-// }
-
-
-// if all ok then show send email animation
-
-// const name = document.getElementById("name");
-// const email = document.getElementById("mail");
-// const subject = document.getElementById("subject");
-// const message = document.getElementById("message");
+// form sent popup and clears fields after validation
 var popup = document.getElementById("snackbar");
 const form = document.getElementById("form");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    form.reset();
     $("#formModal").modal('hide');
     popup.className = "show";
     setTimeout(function(){ popup.className = popup.className.replace("show", ""); }, 4500);
-    console.log("clicked");
 })
